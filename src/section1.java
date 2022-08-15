8c77cc1fb745d1cdafb45b9a207a2aa8ea174a96
Carson <carson.alberding@cdpipelinedevshop.com>
2022-08-15 10:16:16 -0400

    commit 1

---
+++ src/section1.java
@@ -0,0 +1,8 @@
+public class section1 {
+    public static void main(String[] args) {
+
+        //semicolon is critical for java syntax
+        System.out.println("1st java program");
+        //java syntax is case-sensitive
+    }
+    }
